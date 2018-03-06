ccdd39b7bba23022e4b8fa9948fe1584507ccbff
Bec White <white@palantir.net>
2018-03-05 18:06:01 -0600

    UMUS-77: add and remove federated fields; index federated field values.

--- src/SearchApiFederatedSolrField.php
+++ src/SearchApiFederatedSolrField.php
@@ -6,6 +6,7 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
    * @var SearchApiIndex
    */
   protected $index;
+
   /**
    * @var array
    */
@@ -15,50 +16,66 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
    * {@inheritdoc}
    */
   public function alterItems(array &$items) {
-    // TODO: Implement alterItems() method.
+    if (empty($this->options['fields'])) { return; }
+
+    $entity_type = $this->index->getEntityType();
+    $entity_info = entity_get_info($entity_type);
+
+    foreach ($items as &$item) {
+      $id = entity_id($entity_type, $item);
+      $entity = current(entity_load($entity_type, [$id]));
+
+      $bundle = $entity->{$entity_info['entity keys']['bundle']};
+      foreach ($this->options['fields'] as $field) {
+        if (isset($field['bundle'][$bundle])) {
+          $item->{$field['machine_name']} = token_replace($field['bundle'][$bundle], [$entity_type => $entity]);
+        }
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function configurationForm() {
-    $form['fields'] = ['#type' => 'container'];
+    $form['fields'] = [
+      '#type' => 'container',
+      '#prefix' => '<div id="search-api-federated-solr-add-field-settings">',
+      '#suffix' => '</div>',
+    ];
 
-    // Temporarily hard-coded
-    $fields = ['field1'];
-
-    foreach ($fields as $field) {
+    foreach ($this->options['fields'] as $key => $field) {
       $item = [
         '#type' => 'fieldset',
-        '#title' => !empty($this->options['fields'][$field]) ?
-          "{$this->options['fields'][$field]['label']} ({$this->options['fields'][$field]['machine_name']})" : t('New field'),
+        '#title' => !isset($field['is_new']) ?
+          "{$field['label']} ({$field['machine_name']})" : t('New field'),
         '#collapsible' => TRUE,
-        '#collapsed' => isset($this->options['fields'][$field]),
+        '#collapsed' => !isset($field['is_new']),
       ];
       $item['label'] = [
         '#type' => 'textfield',
         '#title' => t('Label'),
-        '#default_value' => $this->options['fields'][$field]['label'],
+        '#default_value' => $field['label'],
       ];
       $item['machine_name'] = [
         '#type' => 'textfield',
         '#title' => t('Machine Name'),
         '#required' => TRUE,
-        '#default_value' => $this->options['fields'][$field]['machine_name'],
+        '#default_value' => $field['machine_name'],
       ];
       $item['multivalue'] = array(
         '#type' => 'radios',
         '#title' => t('Multi-value field'),
         '#options' => array(0 => t('No'), 1 => t('Yes')),
-        '#default_value' => (TRUE === isset($field['multivalue'])) ? $field['multivalue'] : 1,
+        '#default_value' => (TRUE === isset($field['multivalue'])) ? $field['multivalue'] : 0,
         '#required' => TRUE,
-        '#description' => t('Whether the combined field is a multi-valued field'),
+        '#description' => t('Whether this field is a multi-valued field'),
       );
       $item['type'] = array(
         '#type' => 'select',
         '#title' => t('Data type'),
         '#options' => search_api_default_field_types(),
-        '#default_value' => (TRUE === isset($field['type'])) ? $field['type'] : 'integer',
+        '#default_value' => (TRUE === isset($field['type'])) ? $field['type'] : 'string',
         '#required' => TRUE,
         '#description' => t('Data type to save field as'),
       );
@@ -66,6 +83,7 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
         '#type' => 'fieldset',
         '#title' => t('Value to index for each type'),
         '#description' => t('Enter a token or plain text in the field for each type of indexed item.'),
+        '#collapsible' => TRUE,
       ];
 
       $entity_info = entity_get_info($this->index->getEntityType());
@@ -73,11 +91,27 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
         $item['bundle'][$bundle] = [
           '#type' => 'textfield',
           '#title' => "{$bundle_info['label']} ({$bundle})",
-          '#default_value' => $this->options['fields'][$field]['bundle'][$bundle],
+          '#default_value' => $field['bundle'][$bundle],
         ];
       }
 
-      $form['fields']['field1'] = $item;
+      $item['actions'] = array(
+        '#type' => 'actions',
+        'remove' => array(
+          '#type' => 'submit',
+          '#value' => t('Remove field'),
+          '#submit' => array('_search_api_federated_solr_add_field_submit'),
+          '#limit_validation_errors' => array(),
+          '#name' => '_search_api_federated_solr_remove_' . $key,
+          '#ajax' => array(
+            'callback' => '_search_api_federated_solr_add_field_ajax',
+            'wrapper' => 'search-api-federated-solr-add-field-settings',
+          ),
+        ),
+      );
+
+
+      $form['fields'][$key] = $item;
     }
 
     $form['tokens'] = [
@@ -93,7 +127,34 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
       '#recursion_limit' => 2,
     ];
 
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['add_field_federated'] = array(
+      '#type' => 'submit',
+      '#value' => t('Add new federated field'),
+      '#submit' => array('_search_api_federated_solr_add_field_submit'),
+      '#limit_validation_errors' => array(),
+      '#ajax' => array(
+        'callback' => '_search_api_federated_solr_add_field_ajax',
+        'wrapper' => 'search-api-federated-solr-add-field-settings',
+      ),
+    );
+
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configurationFormSubmit(array $form, array &$values, array &$form_state) {
+    foreach ($values['fields'] as $key => $field) {
+      if ($key != $field['machine_name']) {
+        unset($values['fields'][$key]);
+        $values['fields'][$field['machine_name']] = $field;
+      }
+    }
+
+    $this->options = $values;
+    return $values;
   }
 
   /**
@@ -113,5 +174,34 @@ class SearchApiFederatedSolrField extends SearchApiAbstractAlterCallback {
     return $properties;
   }
 
+  /**
+   * Submit helper callback for buttons in the callback's configuration form.
+   */
+  public function formButtonSubmit(array $form, array &$form_state) {
+    $button_name = $form_state['triggering_element']['#name'];
+    if ($button_name == 'op') {
+      $this->options['fields']['_new_field_' . REQUEST_TIME] = ['is_new' => TRUE];
+    }
+    else {
+      $key = substr($button_name, 34);
+      unset($this->options['fields'][$key]);
+    }
+    $form_state['rebuild'] = TRUE;
+    $this->changes = TRUE;
+  }
 
+}
+
+/**
+ * Submit function for buttons in the callback's configuration form.
+ */
+function _search_api_federated_solr_add_field_submit(array $form, array &$form_state) {
+  $form_state['callbacks']['federated_field']->formButtonSubmit($form, $form_state);
+}
+
+/**
+ * AJAX submit function for buttons in the callback's configuration form.
+ */
+function _search_api_federated_solr_add_field_ajax(array $form, array &$form_state) {
+  return $form['callbacks']['settings']['federated_field']['fields'];
 }
