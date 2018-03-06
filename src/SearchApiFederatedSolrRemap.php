1981788402058620b7dd6ab193621be91da2ffb1
Bec White <white@palantir.net>
2018-03-05 20:42:03 -0600

    UMUS-77: add a callback to remap field names.

---
+++ src/SearchApiFederatedSolrRemap.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2018 Palantir.net
+ */
+
+/**
+ * Class SearchApiFederatedSolrRemap
+ * Provides a Search API index data alteration that remaps property names for indexed items.
+ */
+class SearchApiFederatedSolrRemap extends SearchApiAbstractAlterCallback {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function propertyInfo() {
+    $properties = [];
+
+    $fields = $this->index->getFields();
+
+    foreach ($this->options['fields'] as $key => $value) {
+      if ($value && isset($fields[$key])) {
+        $properties[$value] = [
+          'label' => t('@field (remapped from @key)', ['@field' => $fields[$key]['name'], '@key' => $key]),
+          'description' => $fields[$key]['description'],
+          'type' => $fields[$key]['type'],
+        ];
+      }
+    }
+
+    return $properties;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterItems(array &$items) {
+
+    foreach ($items as &$item) {
+      foreach ($this->options['fields'] as $key => $value) {
+        if ($value && isset($item->{$key})) {
+          $item->{$value} = $item->{$key};
+          unset($item->{$key});
+        }
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configurationForm() {
+
+    $form['fields'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Remap properties'),
+      '#description' => t('Enter a machine name to use instead of the original. Blank fields will not be remapped.'),
+    ];
+
+    $fields = $this->index->getFields();
+    foreach ($fields as $key => $field) {
+      $form['fields'][$key] = [
+        '#type' => 'textfield',
+        '#title' => t('@name (%machine_name)', ['@name' => $field['name'], '%machine_name' => $key]),
+        '#default_value' => isset($this->options['fields'][$key]) ? $this->options['fields'][$key] : '',
+      ];
+    }
+
+    return $form;
+  }
+
+}
