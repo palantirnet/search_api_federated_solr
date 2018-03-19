129af066fa39e41d611682832e2af4e0868bc447
Jes Constantine <jesconstantine@gmail.com>
2018-03-19 08:40:16 -0400

    UMUS-32: Add ajax "add more" to federated term config form (BUGGY)
    Allows a user to add another set of source / destination fields to a given bundle's taxonomy term field's fieldset.
    
    There is currently a bug where we see a logic exception error: LogicException: The database connection is not serializable. This probably means you are serializing an object that has an indirect reference to the database connection. Adjust your code so that is not necessary. Alternatively, look at DependencySerializationTrait as a temporary solution. in Drupal\Core\Database\Connection->__sleep() (line 1471 of /var/www/umus.local/web/umichphysd8/docroot/core/lib/Drupal/Core/Database/Connection.php).
    
    Steps to reproduce:
    - Add a new federated term field to the index config
    - Select "categories" from the Article select field
    - Add a taxonomy term to the "source" taxonomy ER field
    - Type in a destination field
    - Click on the "Add another" button
    - Observe that nothing happens
    - Check the browser console for your error
    
    Steps to see the ajax functionality work
    - From the current state (one source/destination term set added)
    - Save the configuration in the modal
    - Select "Done" in the modal
    - Save the configuration on the edit fields page
    - Edit the term field which you just added
    - Click the "Add another" button for the bundle which you just worked with
    - Observe that you can now add more term fieldsets both to this bundle and to other bundles (these new terms, however are not saved to configuration but we can solve that bug next!)
    
    Breakpoints for debugging:
    In an attempt to better understand what is happening:
    - See 2 "// TO TEST: set breakpoint here" comments in the ajax callabck and submit handler methods.
    - See https://www.drupal.org/project/webform/issues/2901697#comment-12230115 for instructions on debugging docroot/core/lib/Drupal/Component/Serialize/PhpSerialize.php to see what data is being serialized (NOTE: I also put a breakpoint on `if (is_array($data)) {` to be able to compare success vs failure data.
    
    Referencing methods in ajax button #ajax['callback'] / #submit callables:
    Based on conversation in a similar workbench_moderation issue (https://www.drupal.org/project/workbench_moderation/issues/2650388#comment-10765238) I landed on using `get_class($this)` but I did also test with functions vs methods (see next commit) with no success.

--- src/Plugin/search_api/processor/Property/FederatedTermProperty.php
+++ src/Plugin/search_api/processor/Property/FederatedTermProperty.php
@@ -46,13 +46,15 @@ class FederatedTermProperty extends ConfigurablePropertyBase {
     foreach ($index->getDatasources() as $datasource_id => $datasource) {
       $bundles     = $datasource->getBundles();
       $entity_type = $datasource->getEntityTypeId();
+      // Define a variable to track number of rows per bundle->taxonomy field.
+      $num_rows = [];
 
       foreach ($bundles as $bundle_id => $bundle_label) {
         $entityManager      = \Drupal::service('entity_field.manager');
         $bundle_fields      = $entityManager->getFieldDefinitions($entity_type, $bundle_id);
         $bundle_taxonomy_field_names = [];
 
-        // Only add entity reference fields with a target type of taxonomy term.
+        // Build array of entity reference fields with a target type of taxonomy term.
         foreach ($bundle_fields as $bundle_field) {
           $bundle_field_type = $bundle_field->getType();
           if ($bundle_field_type === "entity_reference") {
@@ -63,7 +65,7 @@ class FederatedTermProperty extends ConfigurablePropertyBase {
           }
         }
 
-        // Render mapping fields if there are taxonomy term fields.
+        // Create a fieldset per bundle if that bundle has a taxonomy term field.
         if (!empty($bundle_taxonomy_field_names)) {
           // Create a fieldset per bundle with taxonomy term reference fields.
           $form['field_data'][$entity_type][$bundle_id] = [
@@ -93,18 +95,21 @@ class FederatedTermProperty extends ConfigurablePropertyBase {
             '#default_value' => $default,
           ];
 
-          // Remove the "default" taxonomy field name from array, to avoid defining a fieldset for it
+          // Remove the "default" taxonomy field name from array, to avoid defining a fieldset for it.
           unset($bundle_taxonomy_field_names['default']);
 
-          // Define the rest of the form elements dynamically, per taxonomy_field, and only make visible when the respective field is selected.
+          // Define the rest of the form elements dynamically, per taxonomy_field, and only make  visible when the respective field is selected.
           foreach ($bundle_taxonomy_field_names as $bundle_taxonomy_field_id => $bundle_taxonomy_field_name) {
 
-            // Create a fieldset for the selected taxonomy field (this will also serve as the ajax wrapper).
+            // Get the number of fieldset rows per bundle taxonomy field values (maintained in form state).
+            $num_rows[$bundle_id][$bundle_taxonomy_field_id] = $form_state->get($bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows');
+
+            // Create a fieldset for the bundle's selected taxonomy field.
             $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id] = [
               '#type' => 'fieldset',
               '#title' => $this->t($bundle_taxonomy_field_name . ' terms for %bundle', ['%bundle' => $bundle_label]),
               '#fieldset' => 'field_data_' . $entity_type . '_' . $bundle_id,
-              '#prefix' => '<div id="' . $bundle_taxonomy_field_id . '-row-wrapper">',
+              '#prefix' => '<div id="' . $entity_type . '-' . $bundle_id . '-' . $bundle_taxonomy_field_id . '-row-wrapper">',
               '#suffix' => '</div>',
               // Show this fieldset only when the respective field has been chosen from the taxonomy_field select.
               '#states' => [
@@ -116,56 +121,113 @@ class FederatedTermProperty extends ConfigurablePropertyBase {
               ],
             ];
 
-            // Get the target bundle(s) for this field
+            // Get the target bundle(s) for this field.
             $target_bundles = array_values($bundle_fields[$bundle_taxonomy_field_id]->getSettings()['handler_settings']['target_bundles']);
 
-            // Create a taxonomy term entity reference autocomplete tag widget.
-            $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['source_terms'] = [
-              '#fieldset' => $entity_type . '_' . $bundle_id . '_' . $bundle_taxonomy_field_id,
-              '#type' => 'entity_autocomplete',
-              '#target_type' => 'taxonomy_term',
-              '#title' => $this->t('Source ' . $bundle_taxonomy_field_name . ' terms'),
-              '#description' => $this->t('Start typing some ' . $bundle_taxonomy_field_name . ' terms.  You can separate multiple terms with a comma.'),
-              '#default_value' => [],
-              '#tags' => TRUE,
-              '#selection_settings' => [
-                'target_bundles' => $target_bundles,
+            // Render the number of rows based on form state (i.e. has the add one button been pressed?).
+            if (is_null($num_rows[$bundle_id][$bundle_taxonomy_field_id])) {
+              ${$bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows'} = $form_state->set($bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows', 1);
+              $num_rows[$bundle_id][$bundle_taxonomy_field_id] = 1;
+            }
+
+            for ($i = 0; $i < $num_rows[$bundle_id][$bundle_taxonomy_field_id]; $i++) {
+              // Create a taxonomy term entity reference autocomplete tag widget.
+              $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['source_terms'] = [
+                '#fieldset' => $entity_type . '_' . $bundle_id . '_' . $bundle_taxonomy_field_id,
+                '#type' => 'entity_autocomplete',
+                '#target_type' => 'taxonomy_term',
+                '#title' => $this->t('Source ' . $bundle_taxonomy_field_name . ' terms'),
+                '#description' => $this->t('Start typing some ' . $bundle_taxonomy_field_name . ' terms.  You can separate multiple terms with a comma.'),
+                '#default_value' => [],
+                '#tags' => TRUE,
+                '#selection_settings' => [
+                  'target_bundles' => $target_bundles,
+                ],
+              ];
+
+              // Set the default value if something already exists in our config.
+              if (isset($configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['source_terms'])) {
+                // Get the ids from the saved config value.
+                $target_ids = array_map(function ($item) {
+                  return $item['target_id'];
+                }, $configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['source_terms']);
+                // Load the term entity from the id.
+                $term_entities = array_map(function ($tid) {
+                  return Term::load($tid);
+                }, $target_ids);
+
+                // The default value of an entity reference autocomplete field must be an entity object or array of entity objects.
+                $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['source_terms']['#default_value'] = count($term_entities) > 1 ? $term_entities : $term_entities[0];
+              }
+
+              // Create a config text field for the terms' destination value.
+              $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['destination_term'] = [
+                '#fieldset' => $entity_type . '_' . $bundle_id . '_' . $bundle_taxonomy_field_id,
+                '#type' => 'textfield',
+                '#title' => $this->t(' » Destination ' . $bundle_taxonomy_field_name . ' term'),
+                '#description' => $this->t('The value to which the corresponding terms should map.'),
+              ];
+
+              // Set the default value if something already exists in our config.
+              if (isset($configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['destination_term'])) {
+                $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['destination_term']['#default_value'] = $configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id][$i]['destination_term'];
+              }
+            }
+
+            // Create an "add another" button with an ajax callback.
+            $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['add_one'] = [
+              '#name' => $entity_type . '_' . $bundle_id . $bundle_taxonomy_field_id . '_add_one',
+              '#type' => 'submit',
+              '#value' => $this->t('Add another'),
+              '#submit' => [[get_class($this), 'addOne']],
+              '#ajax' => [
+                'callback' => [get_class($this), 'addMoreCallback'],
+                'wrapper' => $entity_type . '-' . $bundle_id . '-' . $bundle_taxonomy_field_id . '-row-wrapper',
               ],
             ];
-
-            // Set the default value if something already exists in our config.
-            if (isset($configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['source_terms'])) {
-              // Get the ids from the saved config value.
-              $target_ids = array_map(function ($item) {
-                return $item['target_id'];
-              }, $configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['source_terms']);
-              // Load the term entity from the id.
-              $term_entities = array_map(function ($tid) {
-                return Term::load($tid);
-              }, $target_ids);
-
-              // The default value of an entity reference autocomplete field must be an entity object or array of entity objects.
-              $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['source_terms']['#default_value'] = count($term_entities) > 1 ? $term_entities : $term_entities[0];
-            }
-
-            // Create a config text field for the terms' destination value.
-            $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['destination_term'] = [
-              '#fieldset' => $entity_type . '_' . $bundle_id . '_field_categories',
-              '#type' => 'textfield',
-              '#title' => $this->t(' » Destination ' . $bundle_taxonomy_field_name . ' term'),
-              '#description' => $this->t('The value to which the corresponding terms should map.'),
-            ];
-
-            // Set the default value if something already exists in our config.
-            if (isset($configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['destination_term'])) {
-              $form['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['destination_term']['#default_value'] = $configuration['field_data'][$entity_type][$bundle_id][$bundle_taxonomy_field_id]['destination_term'];
-            }
           }
         }
       }
     }
 
     return $form;
+  }
+
+  /**
+   * Submit handler for the "add one" button.
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
+  public function addOne(array &$form, FormStateInterface $form_state) {
+    $triggered_element = $form_state->getTriggeringElement();
+    $bundle_id = $triggered_element['#parents'][2];
+    $bundle_taxonomy_field_id = $triggered_element['#parents'][3];
+
+    ${$bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows'} = $form_state->get($bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows');
+    $add_button = ${$bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows'} + 1;
+
+    $form_state->set($bundle_id . '_' . $bundle_taxonomy_field_id . '_num_rows', $add_button);
+    if(true){}; // TO TEST: set breakpoint here.
+    $form_state->setRebuild();
+  }
+
+  /**
+   * Callback for ajax-enabled "add more" button.
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return mixed
+   *   The bundle's taxonomy field fieldset with an extra empty row.
+   */
+  public function addMoreCallback(array &$form, FormStateInterface $form_state) {
+    $triggered_element = $form_state->getTriggeringElement();
+    $entity_id = $triggered_element['#parents'][1];
+    $bundle_id = $triggered_element['#parents'][2];
+    $bundle_taxonomy_field_id = $triggered_element['#parents'][3];
+    if(true){}; // TO TEST:  set breakpoint here.
+    return $form['field_data'][$entity_id][$bundle_id][$bundle_taxonomy_field_id];
   }
 
   /**
@@ -177,11 +239,24 @@ class FederatedTermProperty extends ConfigurablePropertyBase {
 
     $non_empty_values = [];
     // Filter out the data which still has the default value for the taxonomy field select.
-    $non_empty_values['field_data'] = array_map(function(&$entity_type) {
-      return array_filter($entity_type, function(&$bundle_id) {
-        // Unset any child elements with no taxonomy field target_ids
-        foreach($bundle_id as $key=>$value) {
-          if (is_null($value['source_terms'])) {
+    // ['field_data'][$entity_id][$bundle_id][$bundle_taxonomy_field_id][$i].
+    //  ^ map         ^ filter    ^ foreach   ^ foreach + logic + unset  ^ logic + unset.
+    $non_empty_values['field_data'] = array_map(function(&$entity_id) {
+      return array_filter($entity_id, function(&$bundle_id) {
+        // Iterate over each taxonomy field's fieldset.
+        foreach($bundle_id as $key=>&$field) {
+          // Iterate over each fieldset's rows (each will have at least 1).
+          // @todo do not iterate over $field when == "default"
+          foreach($field as $row=>$value) {
+            // Unset any child elements with no taxonomy field target_ids.
+            if (array_key_exists('source_terms', $value) && is_null($value['source_terms'])) {
+              unset($field[$row]);
+            }
+            // Remove the add_one button.
+            unset($field['add_one']);
+          }
+          // Remove any empty taxonomy field names.
+          if (empty($field)) {
             unset($bundle_id[$key]);
           }
         }
