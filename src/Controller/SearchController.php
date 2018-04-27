a19b352cd438664dff188066a5bdfd530252cf97
Jes Constantine <jesconstantine@gmail.com>
2018-04-27 14:48:54 -0400

    UMUS-74: Define controller to render app container + attach libraries

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Drupal\search_api_federated_solr\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the search results page.
+ */
+class SearchController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function searchPage() {
+    $element = array(
+      '#markup' => '<div id="app">Hello, world</div>',
+      '#attached' => array(
+        'library' => array(
+          'search_api_federated_solr/search',
+        ),
+      ),
+    );
+    return $element;
+  }
+
+}
