8779b0efb935b1b7149e4478f6cc339d3de8b3af
Jes Constantine <jesconstantine@gmail.com>
2018-04-28 08:47:42 -0400

    UMUS-74: Prevent Drupal pager styles from bleeding in

--- react-search/src/components/results/pagination.js
+++ react-search/src/components/results/pagination.js
@@ -23,7 +23,7 @@ class FederatedPagination extends React.Component {
   renderPage(page, currentPage, key) {
     let isCurrentPage = page === currentPage;
     return (
-      <li className={cx("pager__item", (isCurrentPage ? 'is-active' : 'not-active'))} key={key}>
+      <li className={cx("search-pager__item", (isCurrentPage ? 'is-active' : 'not-active'))} key={key}>
         <a role="button" tabIndex="0" onClick={this.onPageChange.bind(this, page)} onKeyPress={this.buildHandleEnterKeyPress(this.onPageChange.bind(this, page))} title={isCurrentPage ? "Current page" : `Go to page ${page + 1}`} aria-current={isCurrentPage ? page + 1 : undefined}>
           <span className="element-invisible">Page</span>{page + 1}
         </a>
@@ -59,10 +59,10 @@ class FederatedPagination extends React.Component {
     let lastPageHidden = (pageAmt === 0 || currentPage === pageAmt - 1);
 
     return (
-      <nav className="pager" role="navigation" aria-labelledby="pagination-heading">
+      <nav className="search-pager" role="navigation" aria-labelledby="pagination-heading">
         <h4 id="pagination-heading" className="element-invisible">Pagination</h4>
-        <ul className="pager__items">
-          <li className={cx("pager__item", {"element-invisible": firstPageHidden})} key="start">
+        <ul className="search-pager__items">
+          <li className={cx("search-pager__item", {"element-invisible": firstPageHidden})} key="start">
             <a role="button" tabIndex={firstPageHidden ? "-1" : "0"} onClick={this.onPageChange.bind(this, 0)} onKeyPress={ this.buildHandleEnterKeyPress(this.onPageChange.bind(this, 0)) } title="Go to first page">
               <span className="element-invisible">First page</span>
               <span aria-hidden={firstPageHidden ? "true" : "false"}>
@@ -70,7 +70,7 @@ class FederatedPagination extends React.Component {
               </span>
             </a>
           </li>
-          <li className={cx("pager__item", {"element-invisible": prevPageHidden})} key="prev">
+          <li className={cx("search-pager__item", {"element-invisible": prevPageHidden})} key="prev">
             <a role="button" tabIndex={prevPageHidden ? "-1" : "0"} onClick={this.onPageChange.bind(this, currentPage - 1)} onKeyPress={ this.buildHandleEnterKeyPress(this.onPageChange.bind(this, currentPage - 1)) }title="Go to previous page" rel="next">
               <span className="element-invisible">Previous page</span>
               <span aria-hidden={prevPageHidden  ? "true" : "false"}>
@@ -79,7 +79,7 @@ class FederatedPagination extends React.Component {
             </a>
           </li>
           {pages.map((page, idx) => this.renderPage(page, currentPage, idx))}
-          <li className={cx("pager__item", {"element-invisible": nextPageHidden})} key="next">
+          <li className={cx("search-pager__item", {"element-invisible": nextPageHidden})} key="next">
             <a role="button" tabIndex={nextPageHidden ? "-1" : "0"} onClick={this.onPageChange.bind(this, currentPage + 1, pageAmt)} onKeyPress={ this.buildHandleEnterKeyPress(this.onPageChange.bind(this, currentPage + 1, pageAmt)) } title="Go to next page" rel="next">
               <span className="element-invisible">Next page</span>
               <span aria-hidden={nextPageHidden ? "true" : "false"}>
@@ -87,7 +87,7 @@ class FederatedPagination extends React.Component {
               </span>
             </a>
           </li>
-          <li className={cx("pager__item", {"element-invisible": lastPageHidden})} key="end">
+          <li className={cx("search-pager__item", {"element-invisible": lastPageHidden})} key="end">
             <a role="button" tabIndex={lastPageHidden ? "-1" : "0"} onClick={this.onPageChange.bind(this, pageAmt - 1)} onKeyPress={ this.buildHandleEnterKeyPress(this.onPageChange.bind(this, pageAmt - 1)) } title="Go to last page">
               <span className="element-invisible">Last page</span>
               <span aria-hidden={lastPageHidden ? "true" : "false"}>
