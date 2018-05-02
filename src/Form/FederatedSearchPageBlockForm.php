5558aa427588fb84a3781797caf81eaecd71324e
Jes Constantine <jesconstantine@gmail.com>
2018-05-02 15:58:37 -0400

    UMUS-96: Create a drupal search form vs a twig template generated form

---
+++ src/Form/FederatedSearchPageBlockForm.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\search_api_solr_federated\Form\FederatedSearchPageBlockForm.
+ */
+
+namespace Drupal\search_api_federated_solr\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class FederatedSearchPageForm.
+ *
+ * @package Drupal\search_api_federated_solr\Form
+ */
+class FederatedSearchPageBlockForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'federated_search_page_block_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['q'] = array(
+      '#type' => 'search',
+      '#name' => 'q',
+      '#title' => $this->t('Search'),
+      '#title_display' => 'invisible',
+      '#size' => 15,
+      '#default_value' => '',
+      '#attributes' => array(
+        'title' => $this->t('Enter the terms you wish to search for.'),
+        'placeholder' => '',
+      ),
+      '#prefix' => '<div class="container-inline">',
+    );
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+      '#name' => '',
+      '#suffix' => '</div>',
+    );
+
+    $form['#action'] = $this->getUrlGenerator()->generateFromRoute('search_api_federated_solr.search');
+    $form['#method'] = 'get';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // This form submits to the search page, so processing happens there.
+  }
+
+}
--- src/Plugin/Block/FederatedSearchPageFormBlock.php
+++ src/Plugin/Block/FederatedSearchPageFormBlock.php
@@ -3,65 +3,26 @@ namespace Drupal\search_api_federated_solr\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\BlockPluginInterface;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a "Federated Search Page Form" block.
  *
  * @Block(
- *   id = "federated_search_form_block",
+ *   id = "federated_search_page_form_block",
  *   admin_label = @Translation("Federated Search Page Form block"),
  *   category = @Translation("Federated Search"),
  * )
  */
 class FederatedSearchPageFormBlock extends BlockBase implements BlockPluginInterface {
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-
-    $config = $this->getConfiguration();
-
-    $form['federated_search_page_url'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Search Page URL'),
-      '#description' => $this->t('What is the URL of the federated search page we should redirect to?'),
-      '#default_value' => isset($config['federated_search_page_url']) ? $config['federated_search_page_url'] : '',
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    parent::blockSubmit($form, $form_state);
-    $values = $form_state->getValues();
-    $this->configuration['federated_search_page_url'] = $values['federated_search_page_url'];
-  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $config = $this->getConfiguration();
+    $build = [];
 
-    if (!empty($config['federated_search_page_url'])) {
-      $url = $config['federated_search_page_url'];
-    }
-    else {
-      $url = $this->t('/search-app');
-    }
+    $build['container']['form'] = \Drupal::formBuilder()->getForm('Drupal\search_api_federated_solr\Form\FederatedSearchPageBlockForm');
 
-    return array(
-      '#markup' => $this->t('Hello @url!', array(
-        '@url' => $url,
-      )),
-      '#theme' => 'search_api_federated_solr',
-      '#federated_search_page_url' => $this->configuration['federated_search_page_url'],
-    );
+    return $build;
   }
-
 }
