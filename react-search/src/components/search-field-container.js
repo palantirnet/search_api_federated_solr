43a5af2874fdd0ba924ff94bd40ea96bf99e1ce4
Jes Constantine <jesconstantine@gmail.com>
2018-05-07 13:32:38 -0400

    UMUS-115: Convert tabs to spaces

--- react-search/src/components/search-field-container.js
+++ react-search/src/components/search-field-container.js
@@ -4,34 +4,34 @@ import cx from "classnames";
 
 class FederatedSearchFieldContainer extends React.Component {
 
-	render() {
-		const { onNewSearch } = this.props;
-		return (
-			<div className="search-filters">
+  render() {
+    const { onNewSearch } = this.props;
+    return (
+      <div className="search-filters">
         <button className="search-filters__trigger">Filter Results</button>
         <form className="search-filters__form">
-					<section className="search-accordion" role="region" aria-labelledby="section-title">
+          <section className="search-accordion" role="region" aria-labelledby="section-title">
             <div className="search-filters__row">
               <h2 className="search-filters__title" id="section-title">Filter Results</h2>
             </div>
 
-					<ul className="search-accordion__group">
-						{this.props.children}
-					</ul>
+            <ul className="search-accordion__group">
+              {this.props.children}
+            </ul>
           </section>
 
           <div className="search-filters__row">
             <input className="search-filters__reset" type="button" defaultValue="Clear All" onClick={onNewSearch} />
           </div>
         </form>
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
 
 FederatedSearchFieldContainer.propTypes = {
-	children: PropTypes.array,
-	onNewSearch: PropTypes.func
+  children: PropTypes.array,
+  onNewSearch: PropTypes.func
 };
 
 export default FederatedSearchFieldContainer;
