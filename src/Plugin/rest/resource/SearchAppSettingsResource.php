59bb8619584d1f263fdb7e450b4b98c6ac9545f3
Jes Constantine <jesconstantine@gmail.com>
2018-05-08 11:15:19 -0400

    UMUS-109: Get search_api server url + search site from config

--- src/Plugin/rest/resource/SearchAppSettingsResource.php
+++ src/Plugin/rest/resource/SearchAppSettingsResource.php
@@ -22,7 +22,23 @@ class SearchAppSettingsResource extends ResourceBase {
    * @return \Drupal\rest\ResourceResponse
    */
   public function get() {
-    $response = ['siteSearch' => 'University of Michigan Lab Blog'];
+    $server_config = \Drupal::config('search_api.server.federated_search_index');
+    $server = $server_config->get('backend_config.connector_config');
+    // Get the required server config field data.
+    $server_url = $server['scheme'] . '://' . $server['host'] . ':' . $server['port'];
+    // Check for the non-required server config field data before appending.
+    $server_url .= $server['path'] ?: '';
+    $server_url .= $server['core'] ? '/' . $server['core'] : '';
+    // Append the request handler.
+    $server_url .= '/select';
+
+    $index_config = \Drupal::config('search_api.index.federated_search_index');
+    $site_name = $index_config->get('field_settings.site_name.configuration.site_name');
+
+    $response = [
+      'url' => $server_url,
+      'siteSearch' => $site_name,
+    ];
     return new ResourceResponse($response);
   }
 }
