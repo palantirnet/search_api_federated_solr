813eb2b9bf99eb0dca7373919312ccc35a5c06cb
Jes Constantine <jesconstantine@gmail.com>
2018-05-09 19:25:20 -0400

    UMUS-109: Create search app config form which populates rest resource.

---
+++ src/Form/SearchApiFederatedSolrSearchAppSettingsForm.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\search_api_solr_federated\Form\SearchApiFederatedSolrSearchAppSettingsForm.
+ */
+
+namespace Drupal\search_api_federated_solr\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class SearchApiFederatedSolrSearchAppSettingsForm.
+ *
+ * @package Drupal\search_api_federated_solr\Form
+ */
+class SearchApiFederatedSolrSearchAppSettingsForm extends ConfigFormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'search_api_federated_solr_search_app_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'search_api_federated_solr.search_app.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('search_api_federated_solr.search_app.settings');
+
+    $index_options = [];
+    $search_api_indexes = \Drupal::entityTypeManager()->getStorage('search_api_index')->loadMultiple();
+    /* @var  $search_api_index \Drupal\search_api\IndexInterface */
+    foreach ($search_api_indexes as $search_api_index) {
+      $index_options[$search_api_index->id()] = $search_api_index->label();
+    }
+
+    $form['search_index'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Search API index'),
+      '#description' => $this->t('Defines <a href="/admin/config/search/search-api">which search_api index and server</a> the search app should use.'),
+      '#options' => $index_options,
+      '#default_value' => $config->get('index.id'),
+      '#required' => TRUE,
+      '#ajax' => [
+        'callback' => [$this, 'getSiteName'],
+        'event' => 'change',
+        'wrapper' => 'site-name-property',
+      ],
+    ];
+
+    $form['site_name_property'] = [
+      '#type' => 'hidden',
+      '#attributes' => [
+        'id' => ['site-name-property'],
+      ],
+      '#value' => $config->get('index.has_site_name_property') ? 'true' : '',
+    ];
+
+    $form['set_search_site'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Set the "Site name" facet to this site.'),
+      '#default_value' => $config->get('facet.site_name.set_default'),
+      '#description' => $this
+        ->t('When checked, only search results from this site will be shown, by default, until this site\'s checkbox is unchecked in the search app\'s "Site name" facet.'),
+      '#states' => [
+        'visible' => [
+          ':input[name="site_name_property"]' => [
+            'value' => "true"
+          ],
+        ],
+      ],
+    ];
+
+    $form['#cache'] = ['max-age' => 0];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the search app configuration
+    $config = $this->configFactory->getEditable('search_api_federated_solr.search_app.settings');
+    // Set the search app configuration setting for the default search site flag.
+    $set_search_site = $form_state->getValue('set_search_site');
+    $config->set('facet.site_name.set_default', $set_search_site);
+
+    // Get the id of the chosen index.
+    $search_index = $form_state->getValue('search_index');
+    // Save the selected index option in search app config (for form state).
+    $config->set('index.id', $search_index);
+
+    // Get the index configuration object.
+    $index_config = \Drupal::config('search_api.index.' . $search_index);
+    $site_name_property = $index_config->get('field_settings.site_name.configuration.site_name');
+    $config->set('index.has_site_name_property', $site_name_property ? true : false);
+
+    // Get the id of the chosen index's server.
+    $index_server = $index_config->get('server');
+
+    // Get the server url.
+    $server_config = \Drupal::config('search_api.server.' . $index_server);
+    $server = $server_config->get('backend_config.connector_config');
+    // Get the required server config field data.
+    $server_url = $server['scheme'] . '://' . $server['host'] . ':' . $server['port'];
+    // Check for the non-required server config field data before appending.
+    $server_url .= $server['path'] ?: '';
+    $server_url .= $server['core'] ? '/' . $server['core'] : '';
+    // Append the request handler.
+    $server_url .= '/select';
+
+    // Set the search app configuration setting for the solr backend url.
+    $config->set('index.server_url', $server_url);
+    $config->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+  public function getSiteName(array &$form, FormStateInterface $form_state) {
+    // Get the id of the chosen index.
+    $search_index = $form_state->getValue('search_index');
+    // Get the index configuration object.
+    $index_config = \Drupal::config('search_api.index.' . $search_index);
+    $is_site_name_property = $index_config->get('field_settings.site_name.configuration.site_name') ? 'true' : '';
+
+    $elem = [
+      '#type' => 'hidden',
+      '#name' => 'site_name_property',
+      '#value' => $is_site_name_property,
+      '#attributes' => [
+        'id' => ['site-name-property'],
+      ],
+    ];
+
+    return $elem;
+  }
+}
--- src/Plugin/rest/resource/SearchAppSettingsResource.php
+++ src/Plugin/rest/resource/SearchAppSettingsResource.php
@@ -22,23 +22,39 @@ class SearchAppSettingsResource extends ResourceBase {
    * @return \Drupal\rest\ResourceResponse
    */
   public function get() {
-    $server_config = \Drupal::config('search_api.server.federated_search_index');
-    $server = $server_config->get('backend_config.connector_config');
-    // Get the required server config field data.
-    $server_url = $server['scheme'] . '://' . $server['host'] . ':' . $server['port'];
-    // Check for the non-required server config field data before appending.
-    $server_url .= $server['path'] ?: '';
-    $server_url .= $server['core'] ? '/' . $server['core'] : '';
-    // Append the request handler.
-    $server_url .= '/select';
+    $response_data = [];
 
-    $index_config = \Drupal::config('search_api.index.federated_search_index');
-    $site_name = $index_config->get('field_settings.site_name.configuration.site_name');
+    $config = \Drupal::configFactory()->getEditable('search_api_federated_solr.search_app.settings');
+    $index_config = \Drupal::config('search_api.index.' . $config->get('index.id'));
 
-    $response = [
-      'url' => $server_url,
-      'siteSearch' => $site_name,
-    ];
-    return new ResourceResponse($response);
+    $server_url = $config->get('index.server_url');
+    if ($server_url) {
+      $response_data['url'] = $server_url;
+    }
+
+    // Validate that there is still a site name property set for this index.
+    $site_name_property = $index_config->get('field_settings.site_name.configuration.site_name');
+    $config->set('index.has_site_name_property', $site_name_property ? true : false);
+    // Determine if config option to set default site name is set.
+    $set_default_site = $config->get('facet.site_name.set_default');
+
+    // If default site flag is set and the index has a site property, update response.
+    if ($set_default_site && $site_name_property) {
+      $response_data['siteSearch'] = $site_name_property;
+    }
+    if ($set_default_site && !$site_name_property) {
+      // We no longer have a site name property so unset the set default config.
+      $config->set('facet.site_name.set_default', 0);
+    }
+
+    $config->save();
+
+    $response = new ResourceResponse($response_data);
+
+    // Ensure that when the search app / index config changes, cache for this response is invalidated.
+    $response->addCacheableDependency($config);
+    $response->addCacheableDependency($index_config);
+
+    return $response;
   }
 }
