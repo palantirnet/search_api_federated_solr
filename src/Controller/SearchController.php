66f7fe91e9f83a651bd175e6d0c7dbd5f8ad28ea
Jes Constantine <jesconstantine@gmail.com>
2018-05-09 20:24:41 -0400

    UMUS-87: Use search app twig template to render <noscript/>

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -15,14 +15,14 @@ class SearchController extends ControllerBase {
    *   A simple renderable array.
    */
   public function searchPage() {
-    $element = array(
-      '#markup' => '<div id="root"><noscript>This search page requires Javascript in order to function.  <a href="https://www.whatismybrowser.com/guides/how-to-enable-javascript/auto">Learn how to enable Javascript in your browser.</a></noscript><p class="element-invisible" aria-hidden="true">Federated Solr Search App: If you see this message in your DevTools, it likely means there is an issue adding the app javascript library to this page.  Follow the steps in the search_api_federated_solr module README.</p></div>',
-      '#attached' => array(
-        'library' => array(
+    $element = [
+      '#theme' => 'search_app',
+      '#attached' => [
+        'library' => [
           'search_api_federated_solr/search',
-        ),
-      ),
-    );
+        ],
+      ],
+    ];
     return $element;
   }
 
