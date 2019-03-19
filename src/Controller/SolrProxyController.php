cfd4e8436cfa2ccb50ecbb020a783475f00db3b3
Jes Constantine <jesconstantine@gmail.com>
2019-03-19 12:00:23 -0400

    root-293: stub out query in controller

--- src/Controller/SolrProxyController.php
+++ src/Controller/SolrProxyController.php
@@ -5,16 +5,56 @@ namespace Drupal\search_api_federated_solr\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Cache\CacheableJsonResponse;
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\search_api\Entity\Server;
+use Drupal\search_api\SearchApiException;
+use Drupal\search_api\Query;
 use Symfony\Component\HttpFoundation\Request;
 
 class SolrProxyController extends ControllerBase {
 
   public function getResultsJson(Request $request) {
     $data = [];
-    // Do some useful stuff to build an array of data.
-    $data['response'] = [];
-    $data['response']['docs'] = [];
-    $data['response']['docs'][] = 'hi';
+    // Get index id from search app config.
+    $config = \Drupal::configFactory()->getEditable('search_api_federated_solr.search_app.settings');
+    $index_id = $config->get('index.id');
+    // Get the server id from index config.
+    $index_config = \Drupal::config('search_api.index.' . $index_id);
+    $server_id = $index_config->get('server');
+    // Load the server.
+    /** @var \Drupal\search_api\ServerInterface $server */
+    $server = Server::load($server_id);
+
+    // Get the query data from the qs.
+    $term = 'qui';
+
+    try {
+      /** @var \Drupal\search_api_solr\SolrBackendInterface $backend */
+      $backend = $server->getBackend();
+      $connector = $backend->getSolrConnector();
+      $query = $connector->getSelectQuery();
+      $query->setQuery($term);
+
+        // Build a filter.
+//    $filter = $query->createFilter('OR');
+//    $filter->condition('type', 'article', '=');
+//    $filter->condition('type', 'blog_post', '=');
+//    $query->filter($filter);
+
+        // Conditions.
+//    $query->condition('title_field', $term, '=');
+//    $query->condition('language', $language->language, '=');
+//    $query->sort('timestamp_field');
+
+      // Fetch results.
+      $response = $connector->search($query);
+    }
+    catch (SearchApiException $e) {
+      watchdog_exception('search_api_federated_solr', $e, '%type while executed query on @server: @message in %function (line %line of %file).', array('@server' => $server->label()));
+    }
+
+//    $data['response'] = $results->getExtraData('search_api_solr_response')['response'];
+    $data['response'] = json_decode($response->getBody())->response;
+      if (true){}
 
     // Add Cache settings for Max-age and URL context.
     // You can use any of Drupal's contexts, tags, and time.
