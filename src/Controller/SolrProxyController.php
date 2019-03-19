5cef3358ed75b1fb34e0106de5f14a02afe3681b
Jes Constantine <jesconstantine@gmail.com>
2019-03-19 09:48:40 -0400

    root-863: stub out route + controller for proxy

---
+++ src/Controller/SolrProxyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\search_api_federated_solr\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
+use Symfony\Component\HttpFoundation\Request;
+
+class SolrProxyController extends ControllerBase {
+
+  public function getResultsJson(Request $request) {
+    $data = [];
+    // Do some useful stuff to build an array of data.
+    $data['response'] = [];
+    $data['response']['docs'] = [];
+    $data['response']['docs'][] = 'hi';
+
+    // Add Cache settings for Max-age and URL context.
+    // You can use any of Drupal's contexts, tags, and time.
+    $data['#cache'] = [
+      'max-age' => 600,
+      'contexts' => [
+        'url',
+      ],
+    ];
+    $response = new CacheableJsonResponse($data);
+    $response->addCacheableDependency(CacheableMetadata::createFromRenderArray($data));
+    return $response;
+  }
+}
